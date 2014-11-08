292390c6cfd190e4b8125f411ffa2ba879c3c941
Bradley Skaggs <bskaggs@acm.org>
2014-11-08 14:48:58 -0500

    Craig's changes

--- tokenizer/src/main/java/io/github/clpsych/hackathon/tokenizer/Tokenizer.java
+++ tokenizer/src/main/java/io/github/clpsych/hackathon/tokenizer/Tokenizer.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.Writer;
+import java.io.BufferedWriter;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -30,36 +31,65 @@ import com.google.common.base.Joiner;
 public class Tokenizer {
 	public static ObjectMapper mapper = new ObjectMapper();
 	public static Tagger tagger;
-	
+
 	public static boolean doTokenize = true;
 	public static boolean doTag = true;
 
+	public static BufferedWriter bw;
+
   public static void tokenizeTweets(Reader in, PrintWriter out) throws JsonParseException, IOException {
 	JsonParser parser = mapper.getJsonFactory().createJsonParser(in);
-	
+
 	while (parser.nextToken() != null) {
 		Map<String, Object> tweet = parser.readValueAs(HashMap.class);
 		String text = (String) tweet.get("text");
-		
+
 		if (doTokenize) {
-			List<String> tokenized = Twokenize.tokenize(text);
-			tweet.put("tokenized_text", Joiner.on(" ").join(tokenized));
-		}
-		
-		if (doTag) {
-			List<TaggedToken> taggedTokens = tagger.tokenizeAndTag(text);
-			List<String> taggedTokenStrings = new ArrayList<String>(taggedTokens.size());
-			for (TaggedToken token : taggedTokens) {
-				taggedTokenStrings.add(token.token + "/" + token.tag);
+
+			try {
+				List<String> tokenized = Twokenize.tokenize(text);
+
+				if (tokenized.size()>0)
+				{
+					tweet.put("tokenized_text", Joiner.on(" ").join(tokenized));
+				} else {
+					tweet.put("tokenized_text", "NO_OUTPUT_PRODUCED");
+				}
+			} catch (Throwable t) // yeah, typically you don't want to do this...
+			{
+				bw.write(tweet.get("id").toString());
+				bw.newLine();
 			}
-			tweet.put("tagged_text", Joiner.on(" ").join(taggedTokenStrings));
+		}
+
+		if (doTag) {
+
+			try {
+
+				List<TaggedToken> taggedTokens = tagger.tokenizeAndTag(text);
+				List<String> taggedTokenStrings = new ArrayList<String>(taggedTokens.size());
+				for (TaggedToken token : taggedTokens) {
+					taggedTokenStrings.add(token.token + "/" + token.tag);
+				}
+
+				if (taggedTokenStrings.size() > 0) {
+					tweet.put("tagged_text", Joiner.on(" ").join(taggedTokenStrings));
+				} else {
+					tweet.put("tagged_text", "NO_OUTPUT_PRODUCED");
+				}
+			} catch (Throwable t) // yeah, typically you don't want to do this...
+			{
+				bw.write(tweet.get("id").toString());
+				bw.newLine();
+			}
 		}
 		out.println(mapper.writeValueAsString(tweet));
+
 	}
-  }
-  
-  
-  
+}
+
+
+
   public static void main(String[] args) throws JsonParseException, FileNotFoundException, IOException {
 	final Path inRoot = new File(args[0]).toPath();
 	final Path outRoot = new File(args[1]).toPath();
@@ -68,6 +98,9 @@ public class Tokenizer {
 	if (args.length > 2) {
 		modelFile = args[2];
 	}
+
+	bw = new BufferedWriter(new FileWriter("ignored_tweet_ids.txt"));
+
 	tagger.loadModel(modelFile);
     Files.walkFileTree(inRoot, new SimpleFileVisitor<Path>() {
     	@Override
@@ -84,5 +117,9 @@ public class Tokenizer {
     		return FileVisitResult.CONTINUE;
     	}
     });
+
+		try {bw.close();}
+		catch (IOException ioe) {}
+
   }
 }
