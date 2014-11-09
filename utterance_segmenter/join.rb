146b2fda7ffa82c4f77daba6881d5a65c265143e
Bradley Skaggs <bskaggs@acm.org>
2014-11-09 09:29:14 -0500

    fix for single-word utterances

--- utterance_segmenter/join.rb
+++ utterance_segmenter/join.rb
@@ -20,15 +20,24 @@ while ids.gets
     head = line[6].to_i
     if head > 0
       edges << line[0].to_i << head
-    elsif head == 0
+    elsif head == 0 
+      #make sure you include a link from a root to itself, 
+      #so it appears in the graph if utterance is only one token
       edges << line[0].to_i << line[0].to_i
     end
   end
+  
+  #build graph from dependencies
   g = RGL::DirectedAdjacencyGraph[*edges]
   tree = current_lines.map { |l| [l[1], l[3], l[6]].join("/") }.join(' ')
   utterances = []
-  g.to_undirected.each_connected_component { |c| utterances << c.to_a.sort.map {|i| l = current_lines[i-1]; [l[1], l[3]].join("/") }.join(" ")}
-  
+
+  #pull out connected components
+  g.to_undirected.each_connected_component { |c| utterances << c.to_a.sort }
+  #sort by order of first token
+  utterances = utterances.sort_by { |u| u[0] }
+  #pull out token information for each utterance
+  utterances = utterances..map {|u| u.map { |i| l = current_lines[i-1]; [l[1], l[3]].join("/") }.join(" ")}
   h = {id: current_id, utterances: utterances, tree: tree}
   puts h.to_json 
 end
